155de2294528dfe31ba7b640357cecdd525245e3
peanut996 <peanut996@icloud.com>
2025-04-08 21:16:27 +0800

    add favicon and link it in the layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
+        <head>
+            <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        </head>
         <body>{children}</body>
         </html>
     );
